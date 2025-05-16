b72d23b2ac66c18ad3177dc4c20229e77c4729e9
CXLEB-99 <calebgoforth99@gmail.com>
2025-05-15 20:20:38 -0400

    Updated design of the resume page

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  skills = [
+    'HTML', 'CSS', 'JavaScript', 'Angular', 'Node.js', 'MongoDB', 'Python', 'Git'
+  ];
+}
